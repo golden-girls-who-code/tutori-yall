175ecd1bedee481b1ed8c84e357309de688954e2
rachiebytes <racheljohnson457@gmail.com>
2017-09-17 12:08:11 -0500

    Allow dynamic port binding

--- tutorialsharing/api/run.py
+++ tutorialsharing/api/run.py
@@ -1,4 +1,11 @@
+import os
+
 from tutorialsharing.api.app import app
 
 
-app.run(host='0.0.0.0', port=33507, debug=True)
+port = os.environ.get('PORT')
+if not port:
+    port = 4000
+
+
+app.run(host='0.0.0.0', port=port, debug=True)
